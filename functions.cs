c3b02fd5112370e00be92fb40413cb60142dc7bd
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2018-11-14 20:08:20 +0200

    hi

---
+++ functions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14411
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+
+            Console.WriteLine("doing work...");
+
+        }
+
+        private static void NewMethod()
+        {
+            Console.WriteLine("Enter number for triangle:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
